d007fa6008a87889a5b06a23937b12b01d370ce5
Ivan França <contato.ivanfrancajr@gmail.com>
2025-05-11 13:18:34 -0300

    feat: adiciona arquivo de entrada + arquivo de requisitos

---
+++ main.py
@@ -0,0 +1,9 @@
+#%%
+import tkinter as tk
+from ui.interface_grafica import InterfaceGrafica
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = InterfaceGrafica(root)
+    root.mainloop()
+# %%
